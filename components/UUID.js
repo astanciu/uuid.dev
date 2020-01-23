1a3455038a6b38a76d96425de2bf25d0412a05ab
Alex Stanciu <alex@auth0.com>
2020-01-23 18:57:00 -0500

    fix & upgrade

--- components/UUID.js
+++ components/UUID.js
@@ -4,7 +4,9 @@ const UUID = (props) => {
   const {type, value} = props
   const copy = e => clipboard.writeText(value)
   return (<div className="uuid-container">
-    <div className="uuid-type">{type}</div><div className="uuid-value" onClick={copy}> {value}</div>
+    <div className="uuid-type">{type}</div>
+    <div className="uuid-value" onClick={copy}> {value}</div>
+    <div/>
   </div>)
 }
 
